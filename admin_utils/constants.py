50bf16d176038d62d5eb18c7e33b41f6ffd59e1f
Alexander Demidovskij <monadv@yandex.ru>
2026-01-08 12:48:50 +0300

    CI fixes no. 2

--- admin_utils/constants.py
+++ admin_utils/constants.py
@@ -1,10 +1,30 @@
 """
 Constants for references collection.
 """
+
+# pylint: disable=invalid-name,too-few-public-methods
 try:
     import torch
 except ImportError:
     print('Library "torch" not installed. Failed to import.')
+
+    class torch:  # type: ignore
+        """
+        Mock class for public repo that does not have requirements.
+        """
+
+        class cuda:
+            """
+            Mock class for torch.cuda.
+            """
+
+            @staticmethod
+            def is_available() -> bool:
+                """
+                Mock method for torch.cuda.is_available.
+                """
+                return False
+
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Selected {DEVICE} for all reference collection tasks")
--- admin_utils/precommit.sh
+++ admin_utils/precommit.sh
@@ -2,25 +2,34 @@ set -ex
 
 echo $1
 
-DIRS_TO_CHECK=(
-  "admin_utils"
-  "config"
-  "core_utils"
-  # "lab_7_llm"
-  # "lab_8_sft"
-  # "reference_lab_classification"
-  # "reference_lab_classification_sft"
-  # "reference_lab_generation"
-  # "reference_lab_nli"
-  # "reference_lab_nli_sft"
-  # "reference_lab_nmt"
-  # "reference_lab_nmt_sft"
-  # "reference_lab_ner"
-  # "reference_lab_open_qa"
-  # "reference_lab_summarization"
-  # "reference_lab_summarization_sft"
-  "seminars"
-)
+if [[ "$1" == "public" ]]; then
+  DIRS_TO_CHECK=(
+    "admin_utils"
+    "config"
+    "core_utils"
+    "seminars"
+  )
+else
+  DIRS_TO_CHECK=(
+    "admin_utils"
+    "config"
+    "core_utils"
+    "seminars"
+    "lab_7_llm"
+    "lab_8_sft"
+    "reference_lab_classification"
+    "reference_lab_classification_sft"
+    "reference_lab_generation"
+    "reference_lab_nli"
+    "reference_lab_nli_sft"
+    "reference_lab_nmt"
+    "reference_lab_nmt_sft"
+    "reference_lab_ner"
+    "reference_lab_open_qa"
+    "reference_lab_summarization"
+    "reference_lab_summarization_sft"
+  )
+fi
 
 if [ -d "venv" ]; then
     echo "Taking Python from venv"
@@ -33,13 +42,15 @@ fi
 
 export PYTHONPATH=$(pwd)
 
-# python config/generate_stubs/generate_labs_stubs.py
+if [[ "$1" != "public" ]]; then
+  python config/generate_stubs/generate_labs_stubs.py
+fi
 
-# python -m black "${DIRS_TO_CHECK[@]}"
+python -m black "${DIRS_TO_CHECK[@]}"
 
-# isort .
+isort .
 
-# autoflake -vv .
+autoflake -vv .
 
 python -m pylint "${DIRS_TO_CHECK[@]}"
 
@@ -47,17 +58,20 @@ mypy "${DIRS_TO_CHECK[@]}"
 
 python -m flake8 "${DIRS_TO_CHECK[@]}"
 
-pydoctest --config pydoctest.json
+if [[ "$1" != "public" ]]; then
+  pydoctest --config pydoctest.json
+fi
 
-if [[ "$1" != "smoke" ]]; then
+if [[ "$2" != "smoke" ]]; then
   python config/static_checks/check_doc8.py
 
   python config/static_checks/requirements_check.py
 
-  rm -rf dist
-  sphinx-build -b html -W --keep-going -n . dist -c admin_utils
+  if [[ "$1" != "public" ]]; then
+    rm -rf dist
+    sphinx-build -b html -W --keep-going -n . dist -c admin_utils
+  fi
 
   python -m pytest -m "mark10 and lab_7_llm"
   python -m pytest -m "mark10 and lab_8_sft"
 fi
-
