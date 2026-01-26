7594b407daf6055f1da4837210aff60a449d8ce3
Arina Savina <arinasavina330@gmail.com>
2026-01-26 09:42:05 +0300

    first commit

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -6,6 +6,7 @@ Working with Large Language Models.
 
 # pylint: disable=too-few-public-methods, undefined-variable, too-many-arguments, super-init-not-called
 from typing import Iterable, Sequence
+import pandas
 
 
 class RawDataImporter(AbstractRawDataImporter):
