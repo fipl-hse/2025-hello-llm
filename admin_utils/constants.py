bb907414ef2fef418798feac43e8201d720c8348
Alexander Demidovskij <monadv@yandex.ru>
2026-01-27 08:38:31 +0300

    sync

--- admin_utils/constants.py
+++ admin_utils/constants.py
@@ -31,14 +31,12 @@ except ImportError:
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Selected {DEVICE} for all reference collection tasks")
 
-USE_VENV = True
-
 PROJECT_ROOT = Path(__file__).parent.parent
 PROJECT_CONFIG_PATH = PROJECT_ROOT / "project_config.json"
 CONFIG_PACKAGE_PATH = PROJECT_ROOT / "config"
-CORE_UTILS_PACKAGE_PATH = PROJECT_ROOT / "config"
 TRACKED_JSON_PATH = str(
     (PROJECT_ROOT / "admin_utils" / "external_pr_files" / "tracked_files.json").relative_to(
         PROJECT_ROOT
     )
 )
+DIST_PATH = PROJECT_ROOT / "dist"
--- admin_utils/references/get_sft_references.py
+++ admin_utils/references/get_sft_references.py
@@ -15,6 +15,7 @@ from admin_utils.references.get_model_analytics import get_references, save_refe
 from admin_utils.references.helpers import (
     collect_combinations,
     get_classification_models,
+    get_ner_models,
     get_nli_models,
     get_nmt_models,
     get_summurization_models,
@@ -25,6 +26,7 @@ from core_utils.project.lab_settings import InferenceParams, SFTParams
 
 from reference_lab_classification_sft.start import get_result_for_classification  # isort:skip
 from reference_lab_nli_sft.start import get_result_for_nli  # isort:skip
+from reference_lab_ner_sft.start import get_result_for_ner  # isort:skip
 from reference_lab_nmt_sft.start import get_result_for_nmt  # isort:skip
 from reference_lab_summarization_sft.start import get_result_for_summarization  # isort:skip
 
@@ -40,7 +42,9 @@ class MainParams:
     metrics: list[Metrics]
 
 
-def get_target_modules(model_name: str) -> list[str] | None:
+def get_target_modules(  # pylint: disable=too-many-return-statements)
+    model_name: str,
+) -> list[str] | None:
     """
     Gets modules to fine-tune with LoRA.
 
@@ -70,6 +74,8 @@ def get_target_modules(model_name: str) -> list[str] | None:
         return ["q", "k", "v"]
     if model_name in ("UrukHan/t5-russian-summarization",):
         return ["q", "k", "wi", "wo"]
+    if model_name in ("dslim/distilbert-NER",):
+        return ["q_lin", "k_lin", "v_lin", "out_lin"]
     return None
 
 
@@ -98,6 +104,7 @@ def get_task(
     summarization_models = get_summurization_models()
     nli_models = get_nli_models()
     nmt_models = get_nmt_models()
+    ner_model = get_ner_models()
 
     if model in classification_models:
         fine_tuning_pipeline = get_result_for_classification
@@ -107,6 +114,8 @@ def get_task(
         fine_tuning_pipeline = get_result_for_nli
     elif model in nmt_models:
         fine_tuning_pipeline = get_result_for_nmt
+    elif model in ner_model:
+        fine_tuning_pipeline = get_result_for_ner
     else:
         raise ValueError(f"Unknown model {model} ...")
     return fine_tuning_pipeline(inference_params, sft_params, main_params)
@@ -131,7 +140,7 @@ def main() -> None:
     inference_params = InferenceParams(
         num_samples=100,
         max_length=120,
-        batch_size=64,
+        batch_size=3,
         predictions_path=dist_dir / "predictions.csv",
         device=DEVICE,
     )
@@ -174,19 +183,14 @@ def main() -> None:
 
     result = {}
     for model_name, dataset_name, metrics in tqdm(sorted(combinations)):
-        if (
-            model_name == "cointegrated/rubert-tiny-toxicity"
-            and dataset_name == "OxAISH-AL-LLM/wiki_toxic"
-        ):
-            continue
         print(model_name, dataset_name, metrics, flush=True)
         prepare_result_section(result, model_name, dataset_name, metrics)
 
         sft_params.finetuned_model_path = dist_dir / model_name
         sft_params.learning_rate = specific_lr.get(model_name, 1e-3)
         sft_params.max_fine_tuning_steps = specific_fine_tuning_steps.get(model_name, 50)
-        sft_params.rank = specific_rank.get(model_name, 8)
-        sft_params.alpha = specific_alpha.get(model_name, 8)
+        sft_params.rank = specific_rank.get(model_name, 16)
+        sft_params.alpha = specific_alpha.get(model_name, 16)
         sft_params.target_modules = get_target_modules(model_name)
 
         main_params = MainParams(model_name, dataset_name, [Metrics(metric) for metric in metrics])
