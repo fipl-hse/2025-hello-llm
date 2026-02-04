f1a4c24bb545dd5cd60b8c97adc0df805d814422
Marina Mokina <m.marina.oc14@mail.ru>
2026-02-04 15:23:12 +0300

    again

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -161,7 +161,7 @@ class LLMPipeline(AbstractLLMPipeline):
             return {}
 
         embeddings_length = self._model.config.max_position_embeddings
-        ids = torch.ones(1, embeddings_length, dtype=torch.long)
+        ids = torch.ones(1, int(embeddings_length), dtype=torch.long)
         tokens = {"input_ids": ids, "decoder_input_ids": ids}
 
         result = summary(self._model, input_data=tokens, device=self._device, verbose=0)
