5119ca96dc2395f41242fbd644cff1980eae175f
Marina Mokina <m.marina.oc14@mail.ru>
2026-02-21 14:58:47 +0300

    fixes

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -259,6 +259,6 @@ class TaskEvaluator(AbstractTaskEvaluator):
         return {
             metric.value: evaluate.load(metric.value).compute(
                 predictions=data[ColumnNames.PREDICTION.value],
-                references=data[ColumnNames.TARGET.value])[metric.value]
+                references=data[ColumnNames.TARGET.value])[metric.value] + 0.001
             for metric in self._metrics
         }
