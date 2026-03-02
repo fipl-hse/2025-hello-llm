8e9c102c2a228f0a6f06760c973f0f91c5582f79
Irina Pereyaslavtseva <irinapereyas@gmail.com>
2026-03-02 18:51:24 +0300

    start done

--- lab_8_sft/start.py
+++ lab_8_sft/start.py
@@ -8,7 +8,7 @@ Fine-tuning starter.
 from pathlib import Path
 from core_utils.llm.time_decorator import report_time
 from core_utils.project.lab_settings import LabSettings
-from lab_8_sft.main import RawDataImporter, RawDataPreprocessor
+from lab_8_sft.main import LLMPipeline, RawDataImporter, RawDataPreprocessor, TaskDataset
 
 
 @report_time
@@ -16,7 +16,37 @@ def main() -> None:
     """
     Run the translation pipeline.
     """
-    result = None
+    current_path = Path(__file__).parent
+    settings = LabSettings(current_path / "settings.json")
+    
+    importer = RawDataImporter(settings.parameters.dataset)
+    importer.obtain()
+
+    preprocessor = RawDataPreprocessor(importer.raw_data)
+    preprocessor.transform()
+
+    dataset = TaskDataset(preprocessor.data.head(100))
+    pipeline = LLMPipeline(
+        model_name=settings.parameters.model,
+        dataset=dataset,
+        batch_size=1,
+        max_length=120,
+        device="cpu"
+    )
+
+    model_info = pipeline.analyze_model()
+    for key, value in model_info.items():
+        print(f"{key}: {value}")
+
+    sample = dataset[0]
+    prediction = pipeline.infer_sample(sample)
+
+    print("\nSingle sample inference:")
+    print("Source:", sample[0])
+    print("Target:", sample[1])
+    print("Prediction:", prediction)
+
+    result = prediction
     assert result is not None, "Fine-tuning does not work correctly"
 
 
